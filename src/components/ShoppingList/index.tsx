12e7e8ac2c5102e3d9a840c8bd949b0918388845
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-06-01 11:03:42 -0300

    Ordenando consultas

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -11,6 +11,7 @@ export function ShoppingList() {
   useEffect(() => {
     const subscribe = firestore()
     .collection('products')
+    .orderBy('description')
     .onSnapshot(querySnapshot => {
       const data = querySnapshot.docs.map((doc) => {
         return {

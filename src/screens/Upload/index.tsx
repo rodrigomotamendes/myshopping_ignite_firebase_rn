1151c9f4e11619cd68c1fb0b815d8727f0e07c06
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-06-01 12:21:35 -0300

    Dica para listar arquivos

--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -49,7 +49,7 @@ export function Upload() {
 
   return (
     <Container>
-      <Header title="Lista de compras" />
+      <Header title="Upload de Fotos" />
 
       <Content>
         <Photo uri={image} onPress={handlePickImage} />
